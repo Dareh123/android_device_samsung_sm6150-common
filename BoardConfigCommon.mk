DerpFest-Devices/device_xiaomi_sm6150-common

sm6150-common: Remove quotes in TARGET_RECOVERY_PIXEL_FORMAT
Change-Id: Ic400a47f0b4b8d2884082b99df3fe2e09c9438f6
Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

BoardConfigCommon.mk
@@ -139,7 +139,7 @@ BOARD_USES_QCOM_HARDWARE := true
 # Recovery
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_INCLUDE_RECOVERY_DTBO := true
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 TARGET_RECOVERY_UI_MARGIN_HEIGHT := 35
 
 # Releasetools

Dareh123/android_device_samsung_sm6150-common

BoardConfigCommon.mk
@@ -196,7 +196,7 @@ BOARD_USES_QCOM_HARDWARE := true
 # Recovery
 BOARD_HAS_DOWNLOAD_MODE := true
 BOARD_INCLUDE_RECOVERY_DTBO := true
-TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
+TARGET_RECOVERY_PIXEL_FORMAT := BGRA_8888
 TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/recovery/root/fstab.qcom
 
 # Root